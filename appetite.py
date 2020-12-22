c559c7f2d91bbd27c4746662550f039cf63c26c7
Steven Chica Guerrero <steveguerrero.1994@gmail.com>
2020-12-22 18:13:59 +0100

    Test Commit 2 Back to Roots

--- appetite.py
+++ appetite.py
@@ -2,36 +2,36 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 #
-@app.route('/')
-def hello():
-    return 'Hello'
+#@app.route('/')
+#def hello():
+#    return 'Hello'
 #
 
-#database = {
-#    "Pfannkuchen": ["mehl","eier","milch","zucker"],
-#    "Cabonara": ["spaghetti","speck","eier","parmesan","mehl"]
-#}
+database = {
+    "Pfannkuchen": ["Mehl","Eier","Milch","Zucker"],
+    "Carbonara": ["Spaghetti","Speck","Eier","Parmesan","Mehl"]
+}
 
-#@app.route('/', methods = ['post', 'get'])
-#def index ():
-#    if request.method == 'POST':
-#        ingredient1 = request.form.get('ingredient1')
-#        ingredient1 = ingredient1.lower()
-#        ingredient2 = request.form.get('ingredient2')
-#        ingredient2 = ingredient2.lower()
-#
-#        #print ('Wir haben: ' + ingredient1 + ingredient2)
-#        #age = request.form.get('age')
-#        #print ('Ich bin ' + age + ' Jahre alt.')
-#        meals = []
-#        allIngredients = []
-#        for meal in database:
-#            ingredients = database[meal]
-#            if (ingredient1 in ingredients and ingredient2 in ingredients):
-#                meals.append(meal)
-#                allIngredients.append(ingredients)
+@app.route('/', methods = ['post', 'get'])
+def index ():
+    if request.method == 'POST':
+        ingredient1 = request.form.get('ingredient1')
+        ingredient1 = ingredient1.lower()
+        ingredient2 = request.form.get('ingredient2')
+        ingredient2 = ingredient2.lower()
 
-#    return render_template('home_index.html', meals=meals, ingredients=allIngredients)
+        #print ('Wir haben: ' + ingredient1 + ingredient2)
+        #age = request.form.get('age')
+        #print ('Ich bin ' + age + ' Jahre alt.')
+        meals = []
+        allIngredients = []
+        for meal in database:
+            ingredients = database[meal]
+            if (ingredient1 in ingredients and ingredient2 in ingredients):
+                meals.append(meal)
+                allIngredients.append(ingredients)
+
+    return render_template('home_index.html', meals=meals, ingredients=allIngredients)
 
 if __name__ == '__main__' :
     app.run()
