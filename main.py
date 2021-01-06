95ba560acff61f88226cb0e67006d4dc3ea32c1b
Steven Chica Guerrero <steveguerrero.1994@gmail.com>
2021-01-06 10:09:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import MySQLdb
 app = Flask(__name__)
 app.secret_key = "1234"
 
+#Datenbank credentials werden in die app config eingetragen
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "appetite"
 
+#Datenbank wird mit der App verknüpft
 db = MySQL(app)
 
-
+#Login Funktion
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -23,7 +25,7 @@ def index():
             cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s",
                            (username, password))
             info = cursor.fetchone()
-            print(info);
+            print(info)
             if info is not None:
                 if info['username'] == username and info['password'] == password:
                     session['loginSuccess'] = True
@@ -33,7 +35,7 @@ def index():
 
     return render_template('login.html')
 
-
+#Registrierung Funktion
 @app.route('/new', methods=['GET', 'POST'])
 def new_user():
     if request.method == "POST":
@@ -41,25 +43,25 @@ def new_user():
             username = request.form['username']
             email = request.form['email']
             password = request.form['password']
-            cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
-            cur.execute("INSERT INTO users(username, email, password)VALUES(%s,%s,%s)",
+            cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("INSERT INTO users(username, email, password)VALUES(%s,%s,%s)",
                         (username, email, password))
             db.connection.commit()
             return redirect(url_for('index'))
     return render_template("registration.html")
 
-
+#Ist der user angemeldet dann darf er in die Seite myAccount
 @app.route('/new/profile')
 def profile():
     if session['loginSuccess']:
         return render_template("myAccount.html")
 
-
+#Seite zum Ausloggen
 @app.route('/new/logout')
 def logout():
     session.pop('loginSuccess', None)
     return redirect(url_for('index'))
 
-
+#Hier wird das Python-Programm aufgerufen
 if __name__ == '__main__':
     app.run(debug=True)
