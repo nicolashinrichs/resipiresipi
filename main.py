c31ff17c73b859aa9b1e9265e19e8e9f5c7a542b
miazia <miazia.schueler@hotmail.de>
2021-01-21 16:30:24 +0100

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for
-#from flask_mysqldb import MySQL
-#import MySQLdb
+from flask_mysqldb import MySQL
+import MySQLdb
 
 app = Flask(__name__)
 app.secret_key = "1234"
